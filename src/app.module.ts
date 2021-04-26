a39159868572cdbed7e09863f654ff1c3c121876
diego40g <diegopaznaula@hotmail.com>
2021-04-26 12:33:25 -0500

    typeorm conection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule
+  ],
 })
 export class AppModule {}
